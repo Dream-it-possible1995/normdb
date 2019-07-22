93be55f8a3f9c0ad452d58cc54441f9f9a5923e8
Dream-it-possible1995 <13070139328@163.com>
2019-07-22 10:01:54 +0800

    吸çºæ仙测试项目normdb

---
+++ src/main/java/com/glodon/normdb/NormdbApplication.java
@@ -0,0 +1,13 @@
+package com.glodon.normdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NormdbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NormdbApplication.class, args);
+    }
+
+}
